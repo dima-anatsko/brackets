4df4430a51df7b6448db396c102f8591dbc2e6b2
Dmitry Anatsko <anatskodr@gmail.com>
2021-03-10 14:37:13 +0300

    feat: complete task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    let cache = [];
+    for (let char of str) {
+        for (let i = 0; i < bracketsConfig.length; i++) {
+            if (char === bracketsConfig[i][0]) {
+                if (cache[cache.length - 1] === char) {
+                    cache.pop();
+                } else {
+                    cache.push(bracketsConfig[i][1]);
+                }
+                break;
+            } else if (char === bracketsConfig[i][1]) {
+                if (cache.pop() !== char) {
+                    return false;
+                }
+                break;
+            }
+        }
+    }
+    if (cache.length === 0) {
+        return true;
+    }
+    return false;
+};
